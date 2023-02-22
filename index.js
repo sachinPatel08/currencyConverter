ff2f6294106b63dbeeed3b9262d2611d29c532cc
sachin <sachinpatel.19.cs@iite.indusuni.ac.in>
2023-02-22 17:32:50 +0530

    v-1

---
+++ index.js
@@ -0,0 +1,24 @@
+const axios = require('axios')
+const key = "5ODCy8XHgHblqujC8TBhU20yBXNpCMfv";
+
+
+export const convertMoney = ( from ,to , amount )=>{
+   data(from , to ,amount)
+}
+
+
+const data =async (to ,from ,amount)=>{
+    try {
+        const api =await axios.get(`https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount}`,{
+            headers:{
+                'apikey':key
+            }
+        })
+    console.log(api.data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// convertMoney("INR" , "USD" ,1)
+
