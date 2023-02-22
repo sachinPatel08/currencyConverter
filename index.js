3d04dc82ed276b6a1dac31319e03c0cbf1bb1c5c
sachin <sachinpatel.19.cs@iite.indusuni.ac.in>
2023-02-22 18:03:31 +0530

    add

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 const key = "5ODCy8XHgHblqujC8TBhU20yBXNpCMfv";
 
 
-const convertMoney = ( from ,to , amount )=>{
+export default function convertMoney( from ,to , amount ){
    data(from , to ,amount)
 }
 
@@ -22,4 +22,3 @@ const data =async (to ,from ,amount)=>{
 
 // convertMoney("INR" , "USD" ,1)
 
-module.exports = convertMoney()
