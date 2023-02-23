23827b94ebaf58fdfa4930dc76b85313f9f7ab02
sachin <sachinpatel.19.cs@iite.indusuni.ac.in>
2023-02-23 09:29:56 +0530

    change module

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 const axios = require('axios')
 const key = "5ODCy8XHgHblqujC8TBhU20yBXNpCMfv";
-
-
-
-
-
-export const data =async (to ,from ,amount)=>{
+ exports.data =async function(to ,from ,amount){
     try {
         const api =await axios.get(`https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount}`,{
             headers:{
@@ -17,6 +12,4 @@ export const data =async (to ,from ,amount)=>{
         console.log(error)
     }
 }
-
-// convertMoney("INR" , "USD" ,1)
 
