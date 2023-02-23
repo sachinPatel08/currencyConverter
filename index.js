85ba3c80895a09014131d3a4cfd368f10ff18c14
sachin <sachinpatel.19.cs@iite.indusuni.ac.in>
2023-02-23 09:45:28 +0530

    final change

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const axios = require('axios')
-const key = "5ODCy8XHgHblqujC8TBhU20yBXNpCMfv";
- exports.data =async function(to ,from ,amount){
+const key = require('./config')
+
+exports.data =async function(to ,from ,amount){
     try {
         const api =await axios.get(`https://api.apilayer.com/exchangerates_data/convert?to=${to}&from=${from}&amount=${amount}`,{
             headers:{
-                'apikey':key
+                'apikey':key.config.MY_API_KEY
             }
         })
     console.log(api.data)
